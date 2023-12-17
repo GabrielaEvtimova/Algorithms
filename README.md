43064c5ff4c4c84f007553ad3b2fca97bcc015ff
GabrielaEvtimova <gabriela.evtimova90@gmail.com>
2023-12-17 14:25:15 +0200

    Merge Sort Implementation in Descending Order

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # **Algorithms**
-## Goal - to exercise recursion by implementing some valuable algorithms
+## Goal - to practice recursion by implementing some valuable algorithms
 
 ### 1. Merge Sort
 `Worst Case Time Complexity - O(n log n)`
+
 Merge sort is a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.
--- merge-sort-algorithm.js
+++ merge-sort-algorithm.js
@@ -1,4 +1,9 @@
-const mergeSort = (array) => {
+/**
+ * 
+ * @param {array} array 
+ * @returns sorted arrar in ascending order
+ */
+const mergeSortAscending = (array) => {
 
     // 1. Define the bottom - call the function until reach one element in the array
     if (array.length <= 1) {
@@ -11,8 +16,8 @@ const mergeSort = (array) => {
     const middle = Math.floor(array.length / 2);
 
     // 3. Call the function again and again until it stops
-    const left = mergeSort(array.slice(0, middle));
-    const right = mergeSort(array.slice(middle));
+    const left = mergeSortAscending(array.slice(0, middle));
+    const right = mergeSortAscending(array.slice(middle));
 
     // 4. Process of comparing and merging
 
@@ -38,8 +43,8 @@ const mergeSort = (array) => {
     return [...merged, ...left.slice(leftIndex), ...right.slice(rigthIndex)];
 }
 
-const data = [3, 4, 1, 65, 23, 0, 4, 1, 45, 20, 89, 5, 48];
-console.log(mergeSort(data));
+const numbers = [3, 4, 1, 65, 23, 0, 4, 1, 45, 20, 89, 5, 48];
+console.log(mergeSortAscending(numbers));
 /*
 Expected output:
 [
@@ -49,8 +54,8 @@ Expected output:
 ]
 */
 
-const strings = ['Slipknot', 'Depeche Mode', 'Metallica', 'Deftones', 'Korn', 'System of a Down' ]
-console.log(mergeSort(strings));
+const bands = ['Slipknot', 'Depeche Mode', 'Metallica', 'Deftones', 'Korn', 'System of a Down' ]
+console.log(mergeSortAscending(bands));
 /* 
 Expected output:
 [
@@ -60,5 +65,78 @@ Expected output:
   'Metallica',
   'Slipknot',
   'System of a Down'
+]
+*/
+
+
+
+
+/**
+ * 
+ * @param {array} array 
+ * @returns sorted arrar in descending order
+ */
+const mergeSortDescending = (array) => {
+
+    // 1. Define the bottom - call the function until reach one element in the array
+    if (array.length <= 1) {
+        return array;
+    }
+
+    // 2. Divide 
+
+    // 2.1. Define the middle:
+    const middle = Math.floor(array.length / 2);
+
+    // 3. Call the function again and again until it stops
+    const left = mergeSortDescending(array.slice(0, middle));
+    const right = mergeSortDescending(array.slice(middle));
+
+    // 4. Process of comparing and merging
+
+    // 4.1. Create an empty array to push the sorted elements
+    const merged = [];
+
+    // 4.2. Define start
+    let leftIndex = 0;
+    let rigthIndex = 0;
+
+    // 4.3. Loop to check and sort
+    while(leftIndex < left.length && rigthIndex < right.length) {
+        if (left[leftIndex] > right[rigthIndex]) {
+            merged.push(left[leftIndex]);
+            leftIndex++;
+        } else if (left[leftIndex] <= right[rigthIndex]) {
+            merged.push(right[rigthIndex]);
+            rigthIndex++;
+        }
+    }
+
+    // Return result and merge it with rest of the left and right
+    return [...merged, ...left.slice(leftIndex), ...right.slice(rigthIndex)];
+}
+
+const nums = [3, 4, 1, 65, 23, 0, 4, 1, 45, 20, 89, 5, 48];
+console.log(mergeSortDescending(nums));
+/*
+Expected output:
+[
+  89, 65, 48, 45, 23, 20,
+   5,  4,  4,  3,  1,  1,
+   0
+]
+*/
+
+const metalBands = ['Slipknot', 'Depeche Mode', 'Metallica', 'Deftones', 'Korn', 'System of a Down' ]
+console.log(mergeSortDescending(metalBands));
+/* 
+Expected output:
+[
+  'System of a Down',
+  'Slipknot',
+  'Metallica',
+  'Korn',
+  'Depeche Mode',
+  'Deftones'
 ]
 */
